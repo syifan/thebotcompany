fdd2180691af71df5e033cb3856f9de981633c12
Blake <blake@example.com>
2026-04-13 20:15:27 -0400

    test: run mobile nav regression on narrow chromium viewport

--- monitor/tests/project-go-to-list-mobile.spec.js
+++ monitor/tests/project-go-to-list-mobile.spec.js
@@ -1,7 +1,7 @@
-import { test, expect, devices } from '@playwright/test'
+import { test, expect } from '@playwright/test'
 import { setupMocks, PROJECT_ID, PROJECT_REPO } from './helpers.js'
 
-test.use({ ...devices['iPhone 13'] })
+test.use({ viewport: { width: 390, height: 844 } })
 
 test('mobile back button returns from project page to main list', async ({ page }) => {
   await setupMocks(page)
